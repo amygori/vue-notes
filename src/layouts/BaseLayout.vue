402e88c83816f1ad2523ede10e5e3817a7facc4c
Amy Gori <amydgori@gmail.com>
2023-11-08 12:08:35 -0500

    Add TailwindCSS and initial styling

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="page-layout container">
-      <header>
+  <div class="page-layout">
+      <header class="w-full p-4 bg-cyan-700 text-zinc-50 drop-shadow-md">
         <slot name="header"></slot>
       </header>
-      <main>
+      <main class="m-8">
         <slot></slot>
       </main>
-      <footer>
+      <footer class="container items-center flex flex-col p-4 mx-auto md:p-8 lg:flex-row divide-gray-600 text-smslack text-zinc-400
+      ">
         <slot name="footer"></slot>
       </footer>
   </div>
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      rose: colors.rose,
+      pink: colors.pink,
+      fuchsia: colors.fuchsia,
+      purple: colors.purple,
+      violet: colors.violet,
+      indigo: colors.indigo,
+      blue: colors.blue,
+      sky: colors.sky, // As of Tailwind CSS v2.2, `lightBlue` has been renamed to `sky`
+      cyan: colors.cyan,
+      teal: colors.teal,
+      emerald: colors.emerald,
+      green: colors.green,
+      lime: colors.lime,
+      yellow: colors.yellow,
+      amber: colors.amber,
+      orange: colors.orange,
+      red: colors.red,
+      slate: colors.slate,
+      zinc: colors.zinc,
+      gray: colors.gray,
+      stone: colors.stone
+    },
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
+  }
+}
